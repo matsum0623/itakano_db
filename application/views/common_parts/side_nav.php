1144499921d5ccbe44eb76006c9dc1cd378af8c7
Masanori Matsuura <m.matsuura@pc-link.co.jp>
2017-11-29 13:50:44 +0900

    サイドナビを共通パーツに切り分け

---
+++ application/views/common_parts/side_nav.php
@@ -0,0 +1,18 @@
+<div id="side">
+  <div class="side_box">
+    <div class="side_btn"><a href="contact.html">
+      <div class="bigtxt"><span class="icon-envelope2"></span> お問い合わせ</div>
+      お気軽にご相談下さい</a></div>
+  </div>
+  <h3>Site Menu</h3>
+  <div class="side_box">
+    <ul>
+      <li><a href="index.html">HOME</a></li>
+      <li><a href="gallery.html">商品一覧</a></li>
+      <li><a href="staff.html">スタッフ紹介</a></li>
+      <li><a href="#">店舗案内</a></li>
+      <li><a href="#">アクセス</a></li>
+      <li><a href="contact.html">お問い合わせ</a></li>
+    </ul>
+  </div>
+</div><!--/side end-->
--- application/views/home.php
+++ application/views/home.php
@@ -30,24 +30,7 @@
         <a href="#" class="btn_s"><span class="icon-arrow-right2"></span> 詳細はこちら</a>
       </div>
     </div><!--/content end-->
-    <div id="side">
-      <div class="side_box">
-        <div class="side_btn"><a href="contact.html">
-          <div class="bigtxt"><span class="icon-envelope2"></span> お問い合わせ</div>
-          お気軽にご相談下さい</a></div>
-      </div>
-      <h3>Site Menu</h3>
-      <div class="side_box">
-        <ul>
-          <li><a href="index.html">HOME</a></li>
-          <li><a href="gallery.html">商品一覧</a></li>
-          <li><a href="staff.html">スタッフ紹介</a></li>
-          <li><a href="#">店舗案内</a></li>
-          <li><a href="#">アクセス</a></li>
-          <li><a href="contact.html">お問い合わせ</a></li>
-        </ul>
-      </div>
-    </div><!--/side end-->
+    <?php require_once './common_parts/side_nav.php'; ?>
     <div id="to_top"><a href="#"><span class="icon-arrow-up"></span> ページトップへ戻る</a></div>
   </div><!--/main end-->
 </div><!--/hp_base end-->
